a837032c6bbd0735d3f5a9f05c597ff319c5e5cd
akbaraditamasp <akbaraditamasp@gmail.com>
2024-10-24 15:05:46 +0700

    feat: add start command & dynamic runtime import

--- src/spin.ts
+++ src/spin.ts
@@ -1,10 +1,15 @@
-import { readdirSync } from "fs";
-import { resolve } from "path";
-import http from "./modules/http.js";
-import app from "./modules/app.js";
-import socket from "./modules/socket.js";
-import db from "./modules/db.js";
 import "dotenv/config";
+import { readdirSync } from "fs";
+import { dirname, join } from "path";
+import app from "./modules/app.js";
+import db from "./modules/db.js";
+import http from "./modules/http.js";
+import socket from "./modules/socket.js";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3172;
 
@@ -14,10 +19,11 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3172;
   http.boot(app());
   socket.boot();
 
-  const files = readdirSync(resolve("src", "runtime"));
+  const files = readdirSync(join(__dirname, "runtime"));
 
   for (const file of files) {
-    if (file.endsWith(".ts")) await import(resolve("src", "runtime", file));
+    if (file.endsWith(".ts") || file.endsWith(".js"))
+      await import(join(__dirname, "./runtime", file));
   }
 
   http().listen(port, () => console.log("Server running on port " + port));
